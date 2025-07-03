830a0973b9edcdfd29850dddbcc135861cd5f08e
Serif <seriffkapcuk@gmail.com>
2025-07-03 17:00:52 +0300

    weather app with last updates

--- iOS Path/WeatherAppv2/WeatherAppv2/Controllers/ViewController.swift
+++ iOS Path/WeatherAppv2/WeatherAppv2/Controllers/ViewController.swift
@@ -9,27 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet  var tempResultTextField: UITextField!
-    @IBOutlet  var windSpeedLabel: UILabel!
-    @IBOutlet  var directionOfWind: UILabel!
+    @IBOutlet  var tempResultLabel:UILabel!
+    @IBOutlet  var windSpeedLabel:UILabel!
+    @IBOutlet  var directionOfWind:UILabel!
     @IBOutlet  var searchButton:UISearchBar!
+    @IBOutlet  var showButton:UIButton!
 
-    
-    
     let apiKey = "3fb8728561abe479a2754c2caeb1cc2b"
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tempResultTextField.isUserInteractionEnabled = false
-        
-        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
-            backgroundImage.image = UIImage(named: "background")
-            backgroundImage.contentMode = .scaleAspectFill
-            backgroundImage.clipsToBounds = true
 
-            self.view.insertSubview(backgroundImage, at: 0)
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        updateBackGround()
+        searchButton.placeholder = "Bir şehir gir!"
+        searchButton.barTintColor = .clear
+        searchButton.backgroundImage = UIImage()
+    }
+    
+    func updateBackGround(){
         
+        let now = Date()
+        let calendar = Calendar.current
+        var components = calendar.dateComponents([.year, .month, .day], from: now)
+        components.hour = 13
+        components.minute = 00
+        if let comparison = calendar.date(from: components){
+            if now < comparison{
+                overrideUserInterfaceStyle = .light
+                let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+                backgroundImage.image = UIImage(named: "backgroundmorning")
+                backgroundImage.contentMode = .scaleAspectFill
+                backgroundImage.clipsToBounds = true
+                self.view.insertSubview(backgroundImage, at: 0)
+                showButton.backgroundColor = .lightGray
+            } else {
+                overrideUserInterfaceStyle = .dark
+                let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+                backgroundImage.image = UIImage(named: "backgroundnight")
+                backgroundImage.contentMode = .scaleAspectFill
+                backgroundImage.clipsToBounds = true
+                self.view.insertSubview(backgroundImage, at: 0)
+            }
+        }
     }
     
     @IBAction  func showButtonTapped(_ sender:UIButton){
@@ -57,7 +77,7 @@ class ViewController: UIViewController {
                 let direction = decodedData.wind.deg
                 
                 DispatchQueue.main.async {
-                    self.tempResultTextField.text = "\(temp) °C"
+                    self.tempResultLabel.text = "\(temp) °C"
                     self.windSpeedLabel.text = "\(wind) m/s"
                     self.directionOfWind.text="\(direction)"
                     print("Veri Başarı ile alındı") }
@@ -69,7 +89,5 @@ class ViewController: UIViewController {
         }
         task.resume()
     }
-    
-    
 }
 
